522f87213d69d735ff3a461fefce9e5bfaed3b1a
Licoded <busy.li@foxmail.com>
2021-11-18 10:39:33 +0800

    feat(优化): sourcemap方便DEBUG，build前清空

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,9 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
